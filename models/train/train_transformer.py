97fd510e458fdb31f26b1c1154247045179ef82b
Belinda Zou Li <26785408+belindal@users.noreply.github.com>
2021-11-28 13:08:31 -0500

    no explicit alignment during train

--- models/train/train_transformer.py
+++ models/train/train_transformer.py
@@ -28,7 +28,6 @@ from tqdm import tqdm
 # python models/train/train_transformer.py --data data/json_feat_2.1.0 --model seq2seq_im_mask --dout exp/model:{model},name:pm_and_subgoals_01 --splits data/splits/oct21.json --gpu --batch 8 --pm_aux_loss_wt 0.1 --subgoal_aux_loss_wt 0.1
 class ALFREDDataloader(DataLoader):
     def __init__(self, args, vocab, dataset, split_type, batch_size, sep_actions: bool):
-        super().__init__(dataset, batch_size, collate_fn=self.collate_fn)
         self.vocab = vocab
         self.args = args
         # params
@@ -40,7 +39,7 @@ class ALFREDDataloader(DataLoader):
         self.vis_encoder = ResnetVisualEncoder(dframe=self.image_input_dim)
         for param in self.vis_encoder.parameters():
             param.requires_grad = False
-        self.dataset = [self.load_task_json(task, args.data, args.pp_folder) for task in self.dataset]
+        dataset = [self.load_task_json(task, args.data, args.pp_folder) for task in dataset]
 
         if "train" in split_type or "valid" in split_type:
             # self.dataset = self.featurize(self.dataset)
@@ -70,7 +69,7 @@ class ALFREDDataloader(DataLoader):
                 all_frames = self.vis_encoder(all_frames)
 
                 assert len(ex['num']['lang_instr']) == len(ex['num']['action_low'])
-                prev_subgoals = goal
+                prev_subgoals = []
                 prev_actions = []
                 prev_action_masks = []
                 action_idx = 0
@@ -80,33 +79,22 @@ class ALFREDDataloader(DataLoader):
                     # subgoal_actions = self.vocab['action_high'].index2word(ex['num']['action_high'][subgoal_idx]['action'])
                     for action in ex['num']['action_low'][subgoal_idx]:
                         action_low = self.vocab['action_low'].index2word(action['action'])
-                        # unravel actions
-                        if sep_actions:
-                            new_ex = {
-                                'goal': goal,
-                                'curr_subgoal': subgoal,
-                                'prev_subgoals': [sg for sg in prev_subgoals],
-                                'curr_state': all_frames[action_idx],
-                                'prev_actions': [a for a in prev_actions],
-                                'curr_action': action_low,
-                            }
-                            if action['mask'] is not None: new_ex['curr_action_mask'] = self.decompress_mask(action['mask'])
-                            new_dataset.append(new_ex)
                         if action['mask'] is not None: prev_action_masks.append(self.decompress_mask(action['mask']))
                         prev_actions.append(action_low)
                         action_idx += 1
-                    if not sep_actions:
-                        new_ex = {
-                            'goal': goal,
-                            'curr_subgoal': subgoal,
-                            'prev_subgoals': [sg for sg in prev_subgoals],
-                            'all_states': all_frames,
-                            'all_actions': [a for a in prev_actions],
-                        }
-                        if len(prev_action_masks) > 0: new_ex['all_actions_mask'] = prev_action_masks
-                        new_dataset.append(new_ex)
+                    new_ex = {
+                        'goal': goal,
+                        'curr_subgoal': subgoal,
+                        'prev_subgoals': [sg for sg in prev_subgoals],
+                        'all_subgoals': ex['num']['lang_instr'],
+                        'all_states': all_frames,
+                        'all_actions': [a for a in prev_actions],
+                    }
+                    if len(prev_action_masks) > 0: new_ex['all_actions_mask'] = prev_action_masks
+                    new_dataset.append(new_ex)
                     prev_subgoals += subgoal
-                self.dataset = new_dataset
+                dataset = new_dataset
+        super().__init__(dataset, batch_size, collate_fn=self.collate_fn)
 
 
     def decompress_mask(self, compressed_mask):
@@ -133,112 +121,6 @@ class ALFREDDataloader(DataLoader):
             # if not self.test_mode:
             feat['num']['action_low'] = [a for a_group in feat['num']['action_low'] for a in a_group]
 
-    def featurize(self, batch, load_mask=True, load_frames=True):
-        '''
-        tensorize and pad batch input
-        '''
-        device = torch.device('cuda') if self.args.gpu else torch.device('cpu')
-        feat = collections.defaultdict(list)
-
-        for ex in batch:
-            ###########
-            # auxillary
-            ###########
-            # subgoal completion supervision
-            if self.args.subgoal_aux_loss_wt > 0:
-                feat['subgoals_completed'].append(np.array(ex['num']['low_to_high_idx']) / self.max_subgoals)
-
-            # progress monitor supervision
-            if self.args.pm_aux_loss_wt > 0:
-                num_actions = len([a for sg in ex['num']['action_low'] for a in sg])
-                subgoal_progress = [(i+1)/float(num_actions) for i in range(num_actions)]
-                feat['subgoal_progress'].append(subgoal_progress)
-
-            #########
-            # inputs
-            #########
-            # serialize segments
-            self.serialize_lang_action(ex)
-
-            # goal and instr language
-            lang_goal, lang_instr = ex['num']['lang_goal'], ex['num']['lang_instr']
-
-            # # zero inputs if specified
-            # lang_goal = self.zero_input(lang_goal) if self.args.zero_goal else lang_goal
-            # lang_instr = self.zero_input(lang_instr) if self.args.zero_instr else lang_instr
-
-            # append goal + instr
-            lang_goal_instr = lang_goal + lang_instr
-            feat['lang_goal_instr'].append(lang_goal_instr)
-
-            # load Resnet features from disk
-            if load_frames:   #and not self.test_mode:
-                root = self.get_task_root(ex)
-                im = torch.load(os.path.join(root, self.feat_pt))
-
-                num_low_actions = len(ex['plan']['low_actions']) + 1  # +1 for additional stop action
-                num_feat_frames = im.shape[0]
-
-                # Modeling Quickstart (without filler frames)
-                if num_low_actions == num_feat_frames:
-                    feat['frames'].append(im)
-
-                # Full Dataset (contains filler frames)
-                else:
-                    keep = [None] * num_low_actions
-                    for i, d in enumerate(ex['images']):
-                        # only add frames linked with low-level actions (i.e. skip filler frames like smooth rotations and dish washing)
-                        if keep[d['low_idx']] is None:
-                            keep[d['low_idx']] = im[i]
-                    keep[-1] = im[-1]  # stop frame
-                    feat['frames'].append(torch.stack(keep, dim=0))
-
-            #########
-            # outputs
-            #########
-            # low-level action
-            feat['action_low'].append([a['action'] for a in ex['num']['action_low']])
-
-            # low-level action mask
-            if load_mask:
-                feat['action_low_mask'].append([self.decompress_mask(a['mask']) for a in ex['num']['action_low'] if a['mask'] is not None])
-
-            # low-level valid interact
-            feat['action_low_valid_interact'].append([a['valid_interact'] for a in ex['num']['action_low']])
-
-        # tensorization and padding
-        for k, v in feat.items():
-            if k in {'lang_goal_instr', 'action_low', 'action_high'}:
-                # language embedding and padding
-                seqs = [' '.join(self.vocab['word'].index2word(vv)) for vv in v]
-                # seqs = self.tokenizer(seqs, return_tensors='pt', padding=True).to(device)
-                # pad_seq = pad_sequence(seqs, batch_first=True, padding_value=self.pad)
-                # seq_lengths = np.array(list(map(len, v)))
-                # # TODO self.vocab
-                # embed_seq = self.emb_word(pad_seq)
-                # packed_input = pack_padded_sequence(embed_seq, seq_lengths, batch_first=True, enforce_sorted=False)
-                feat[k] = seqs
-            elif k in {'action_low_mask'}:
-                continue
-                # seqs = [torch.tensor(vv, device=device, dtype=torch.float) for vv in v]
-                # feat[k] = seqs
-            elif k in {'subgoal_progress', 'subgoals_completed'}:
-                # auxillary padding
-                seqs = [torch.tensor(
-                    vv, #device=device,
-                    dtype=torch.float) for vv in v]
-                pad_seq = pad_sequence(seqs, batch_first=True, padding_value=self.pad)
-                feat[k] = pad_seq
-            else:
-                # default: tensorize and pad sequence
-                seqs = [torch.tensor(
-                    vv, #device=device,
-                    dtype=torch.float if ('frames' in k) else torch.long) for vv in v]
-                pad_seq = pad_sequence(seqs, batch_first=True, padding_value=self.pad)
-                feat[k] = pad_seq
-
-        return feat
-
     def load_task_json(self, task, data_path, pp_folder):
         '''
         load preprocessed json from disk
@@ -247,7 +129,7 @@ class ALFREDDataloader(DataLoader):
         with open(json_path) as f:
             data = json.load(f)
         return data
-    
+
     def pad_stack(self, data: list):
         max_item_len = max([len(item) for item in data])
         data = [torch.cat([
@@ -256,12 +138,6 @@ class ALFREDDataloader(DataLoader):
         return torch.stack(data)
 
     def collate_fn(self, batch):
-        # for i in trange(0, len(data), batch_size, desc='batch'):
-        # tasks = data[i:i+batch_size]
-        # batch = [self.load_task_json(task) for task in batch]
-        # for task in batch:
-        #     assert len(task['ann']['instr']) == len(task['plan']['high_pddl'])
-        # feat = self.featurize(batch)
         device = torch.device('cuda') if self.args.gpu else torch.device('cpu')
         if 'prev_actions' in batch[0]:
             feat = {
@@ -279,47 +155,29 @@ class ALFREDDataloader(DataLoader):
                 'all_actions_mask': [],
             }
         for item in batch:
-            # 'goal', 'curr_subgoal', 'prev_subgoals', 'curr_state', 'prev_actions', 'curr_action', 'curr_action_mask'
-            # 'goal', 'curr_subgoal', 'prev_subgoals', 'all_states', 'all_actions', 'all_actions_mask'
-            feat['input_goals'].append(' '.join(item['prev_subgoals'] + item['curr_subgoal']))
-            if 'prev_actions' in item:
-                feat['curr_state'].append(item['curr_state'].to(device))
-                feat['prev_actions'].append(' '.join(item['prev_actions']))
-                feat['tgt_action'].append(' '.join(item['curr_action']))
-                if 'curr_action_mask' in item:
-                    feat['tgt_action_mask'].append(item['curr_action_mask'].to(device))
-            else:
-                assert 'all_actions' in item
-                # expand `all_states` for each token of actions
-                all_states_expanded = []
-                # TODO unroll `actions_mask` with this
-                for action_idx in range(len(item['all_actions'])):
-                    for _ in self.tokenizer.tokenize(item['all_actions'][action_idx]):
-                        all_states_expanded.append(item['all_states'][action_idx])
-                assert len(all_states_expanded) == len(self.tokenizer.tokenize(' '.join(item['all_actions'])))
-                # (n_actions, 512, width, height)
-                feat['all_states'].append(torch.stack(all_states_expanded).to(device))
-                feat['all_actions'].append(' '.join(item['all_actions']))
-                if 'all_actions_mask' in item:
-                    feat['all_actions_mask'].append(
-                        torch.cat([mask.to(device) for mask in item['all_actions_mask']])
-                    )
+            # 'goal', 'curr_subgoal', 'prev_subgoals', 'all_subgoals', 'all_states', 'all_actions', 'all_actions_mask'
+            # if gt_alignment:
+            #     feat['input_goals'].append(' '.join(item['goal'] + item['prev_subgoals'] + item['curr_subgoal']))
+            feat['input_goals'].append(' '.join(item['goal'] + item['all_subgoals']))
+            # expand `all_states` for each token of actions
+            all_states_expanded = []
+            for action_idx in range(len(item['all_actions'])):
+                for _ in self.tokenizer.tokenize(item['all_actions'][action_idx]):
+                    all_states_expanded.append(item['all_states'][action_idx])
+            assert len(all_states_expanded) == len(self.tokenizer.tokenize(' '.join(item['all_actions'])))
+            # (n_actions, 512, width, height)
+            feat['all_states'].append(torch.stack(all_states_expanded).to(device))
+            feat['all_actions'].append(' '.join(item['all_actions']))
+            if 'all_actions_mask' in item:
+                feat['all_actions_mask'].append(
+                    torch.cat([mask.to(device) for mask in item['all_actions_mask']])
+                )
         for key in feat:
             if 'state' not in key and 'mask' not in key:
                 feat[key] = self.tokenizer(feat[key], return_tensors='pt', padding=True).to(device)
             else:
                 feat[key] = self.pad_stack(feat[key])
         return batch, feat
-
-    # def iterate(self, data, batch_size):
-    #     '''
-    #     breaks dataset into batch_size chunks for training
-    #     '''
-    #     for i in trange(0, len(data), batch_size, desc='batch'):
-    #         tasks = data[i:i+batch_size]
-    #         batch = [self.load_task_json(task) for task in tasks]
-    #         feat = self.featurize(batch)
-    #         yield batch, feat
 
 
 def compute_metric(self, preds, data):
@@ -383,7 +241,7 @@ if __name__ == '__main__':
     # debugging
     parser.add_argument('--fast_epoch', help='fast epoch during debugging', action='store_true')
     parser.add_argument('--dataset_fraction', help='use fraction of the dataset for debugging (0 indicates full size)', default=0, type=int)
-    
+
     # args and init
     args = parser.parse_args()
     args.dout = args.dout.format(**vars(args))
@@ -425,10 +283,10 @@ if __name__ == '__main__':
         else:
             splits[split_type] = splits[split_type][:1000]
         dl_splits[split_type] = ALFREDDataloader(args, vocab, splits[split_type], split_type, args.batch, sep_actions)
-    
+
     # load model
     # model = T5ForConditionalGeneration.from_pretrained('t5-small').to('cuda')
-    model = GoalConditionedTransformer()
+    model = GoalConditionedTransformer(args=args)
     if args.gpu:
         model = model.to('cuda')
     all_parameters = [p for p in model.parameters() if p.requires_grad]
@@ -458,8 +316,8 @@ if __name__ == '__main__':
             pbar.set_description(f"training loss: {loss.item()}")
             loss.backward()
             optimizer.step()
-
         # evaluate
         for batch, feat in dl_splits['valid_seen']:
             #TODO
             continue
+    torch.save(model.state_dict(), "models/pretrained/transformer.pth")
